c7e6b71429b9b304cea837b5d95ad0c1050641e7
Dan Field <dof@Dans-Mac-mini.home>
2015-09-29 16:48:34 +0100

    initial passing of form parameters to action

---
+++ plugins/nlwCirculationPlugin/modules/nlwCirculationPlugin/templates/makeRequestSuccess.php
@@ -0,0 +1,13 @@
+<?php decorate_with('layout_2col'); ?>
+
+<?php slot('sidebar') ?>
+
+<?php slot('title') ?>
+  <h1><?php echo __('Request Item'); ?></h1>
+<?php end_slot() ?>
+
+<?php slot('content') ?>
+
+<?php echo $sf_request->getParameter('shib_user'); ?>
+
+<?php end_slot() ?>
