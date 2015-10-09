670ff91292cf5a5a391c3903967b0917bbe264b5
Dan Field <surfrdan@gmail.com>
2015-10-09 12:25:17 +0100

    added request editing staff interface and slips printing

---
+++ plugins/nlwCirculationPlugin/modules/nlwCirculationPlugin/templates/printRequestSuccess.php
@@ -0,0 +1,11 @@
+<?php decorate_with('layout_2col'); ?>
+
+<?php slot('sidebar') ?>
+
+<?php slot('title') ?>
+  <h1><?php echo __('Print Request'); ?></h1>
+<?php end_slot() ?>
+
+<?php slot('content') ?>
+
+<?php end_slot() ?>
